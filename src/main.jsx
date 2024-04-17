af19f27ec66ec9e5c84aab4ce59a48f56d0a58ca
fansoa <fverin.contactpro@gmail.com>
2024-04-17 22:47:37 +0200

    💄: add GlobalStyles

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from "@/src/App";
 import "./normalize.css";
 import theme from "@/src/theme/theme";
 import "@/services/i18next/initTranslation";
+import GlobalStyles from "@/src/theme/sharedStyles/components/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyles/>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
---
+++ src/theme/sharedStyles/components/GlobalStyles/index.jsx
@@ -0,0 +1,6 @@
+import SHARED_STYLES from "@/src/theme/sharedStyles/constants/SHARED_STYLES";
+import { Global } from "@emotion/react";
+
+const GlobalStyles = () => <Global styles={SHARED_STYLES} />;
+
+export default GlobalStyles;
