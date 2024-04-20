956a1a919922f82c2d7406e48232b22910868c76
fansoa <fverin.contactpro@gmail.com>
2024-04-20 14:39:11 +0200

    💄: use color provided by ThemeProvider instead of static global

--- src/pages/layout/styled.js
+++ src/pages/layout/styled.js
@@ -1,14 +1,15 @@
 import styled from "@emotion/styled";
-import global from "@/src/theme/global";
 
 const Container = styled.div`
-  background: linear-gradient(
-    180deg,
-    ${global.colors.sandyBrown} 0%,
-    ${global.colors.darkSlateGray} 100%
-  );
-  min-height: 100vh;
-  height: 100%;
+  ${({ theme }) => `
+    background: linear-gradient(
+      180deg,
+      ${theme.colors.sandyBrown} 0%,
+      ${theme.colors.darkJungleGreen} 100%
+    );
+    min-height: 100vh;
+    height: 100%;
+  `}
 `;
 
 export default Container;
