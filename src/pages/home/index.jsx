5e738c4a76363f694f6e770f5065fe6e572a582c
fansoa <fverin.contactpro@gmail.com>
2024-04-20 15:58:16 +0200

    ✨(Home): add page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import PokemonForm from "@/src/pages/home/components/PokemonForm";
+import CustomLayout from "@/src/pages/home/styled";
+
+const HomePage = () => {
+  return (
+    <CustomLayout>
+      <PokemonForm />
+    </CustomLayout>
+  );
+};
+
+export default HomePage;
