8703f3887372f771e93290fe4efa6fa599fe21e3
fansoa <fverin.contactpro@gmail.com>
2024-04-25 18:46:29 +0200

    ✨: add Type

---
+++ src/pages/PokemonDetails/components/Type/Type.stories.jsx
@@ -0,0 +1,10 @@
+import Type from "@/src/pages/PokemonDetails/components/Type";
+
+export default {
+  title: "Components/Type",
+  component: Type,
+};
+
+export const TypeStory = {
+  name: "Default",
+};
